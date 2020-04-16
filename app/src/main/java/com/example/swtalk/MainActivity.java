ff7f45a63af0ef252654835e2ec015bb9d6d2834
ksw5126 <ksw3149@naver.com>
2020-04-16 18:09:31 +0900

    #8 FragMent storage!

--- app/src/main/java/com/example/swtalk/MainActivity.java
+++ app/src/main/java/com/example/swtalk/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.swtalk.fragment.PeopleFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.mainactivity_framelayout, new PeopleFragment()).commit();
     }
 }
